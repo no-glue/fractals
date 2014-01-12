fedb2fe7d7a408030c519cb06ce7d65cdb0fad8f
Nikola Pavlicevic <nikola_pav@eunet.rs>
2014-01-12 14:21:49 +0100

    includes + basic canvas

---
+++ includes/chaos.js
@@ -0,0 +1,33 @@
+// includes for working with canvas
+// basic stuff like set width and height and clear rect
+var ChaosFactory = function(canvas, width, height) {
+  var Chaos = function(canvas, width, height) {
+    var root = this;
+
+    root.canvas = canvas;
+
+    root.width = root.canvas.width = width;
+
+    root.height = root.canvas.height = height;
+
+    root.context = root.canvas.getContext('2d');
+
+    root.setSize = function(width, height) {
+      root.width = root.canvas.width = width;
+
+      root.height = root.canvas.height = height;
+    };
+
+    root.clear = function(color) {
+      if(color) {
+        root.content.fillStyle = color;
+
+        root.context.fillRect(0, 0, root.width, root.height);
+      } else {
+        root.context.clearRect(0, 0, root.width, root.height);
+      }
+    };
+  };
+
+  return new Chaos(canvas, width, height);
+};
