4db8d89d36a919cb78741f80e945f0f395df4d3a
no-glue <nikola_pav@eunet.rs>
2014-03-01 18:18:04 +0100

    string graphics

---
+++ strings_graphics/index.js
@@ -0,0 +1,91 @@
+var Image = function(
+  maxIter
+) {
+  var root = this;
+
+  root.maxIter = maxIter;
+
+  root.space = function() {
+    root.maxIter *= 2;
+  };
+
+  root.draw = function(chaos) {
+    var vocabulary = '+-FXY', 
+    seed = 'FX', 
+    transformedString = '',
+    rules = {}, 
+    commands = {}, 
+    ch = '',
+    x = chaos.width * 0.5, 
+    y = chaos.height * 0.5, 
+    size = 16, 
+    angle = 0, 
+    turnAngle = 90;
+
+    rules['X'] = 'X+YF+';
+
+    rules['Y'] = '-FX-Y';
+
+    for(var iter = 0; iter < root.maxIter; iter++) {
+      transformedString = '';
+
+      for(var transform = 0; transform < seed.length; transform++) {
+        transformedString += ((ch = rules[seed.charAt(transform)])) ? ch : '';
+      }
+
+      seed = transformedString;
+    }
+
+    chaos.clear();
+
+    chaos.context.beginPath();
+
+    console.log('seed>>>', seed);
+
+    chaos.context.moveTo(x, y);
+
+    for(var at = 0, len = seed.length; at < len; at++) {
+      ch = seed.charAt(at);
+
+      switch(ch) {
+        case 'F': 
+          x += Math.cos(angle * Math.PI / 180) * size;
+          y += Math.sin(angle * Math.PI / 180) * size;
+          console.log('F>>>', x, Math.cos(angle * Math.PI / 180) * size, angle, y);
+          break;
+        case '+': angle += turnAngle; console.log('+', angle); break;
+        case '-': angle -= turnAngle; console.log('-', angle); break;
+      }
+
+      chaos.context.lineTo(x, y);
+    }
+
+    chaos.context.stroke();
+  };
+
+  root.doDrawing = function(chaos, depth) {
+  };
+
+  root.restore = function(chaos) {
+    chaos.context.restore();
+  };
+
+  root.getMetric = function() {
+    return root.maxDepth;
+  };
+};
+
+var chaos = ChaosFactory(
+  document.getElementById('canvas'),
+  window.innerWidth,
+  window.innerHeight
+);
+
+// - rotate right
+// + rotate left
+var keys = KeysFactory(
+  chaos,
+  new Image(
+    2
+  )
+);
